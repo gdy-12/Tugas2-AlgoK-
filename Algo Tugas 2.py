5848adfcd5d84a182c311a94367d1006c410dabc
gdy-12 <gungdeyudi880@gmail.com>
2024-12-08 10:30:36 +0800

    Add files via upload

---
+++ Algo Tugas 2.py
@@ -0,0 +1,12 @@
+print ("Konversi Nilai Meter ke Centimeter")
+
+#input nilai meter yang akan dikonversi
+meter = float(input("Masukkan Nilai meter: "))
+
+#Proses Konversi meter ke centimeter
+centimeter = meter * 100 
+hasil =("Panjang Dalam centimeter: " + str(centimeter) + "cm")
+
+#Hasil Konversi
+print(hasil)              
+              
